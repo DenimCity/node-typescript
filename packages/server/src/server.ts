f6ebda4a7ad9ce23a510a3d4a9d57976ced7d952
jxa8hst <jean_altidor@homdepot.com>
2019-09-11 13:03:04 -0400

    brought in dotenv

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,27 +1,24 @@
-import express, {Express} from 'express';
+import 'dotenv/config';
+import express, { Express } from 'express';
 import logger from './utils/logger';
-import routes from './routes'
-
+import routes from './routes';
 
 const server: Express = express();
 const router = express.Router();
 const PORT = process.env.PORT || 4000;
 
-
- server
- .use(express.json())
- .use((req, res, next) => {
-    res.append('Access-Control-Allow-Methods', 'OPTIONS');
-    res.append('Cache-Control', 'no-store');
-    next();
-  })
- .use(router)
- .use('/api', routes)
-
- .use((req, res, next) => {
-    res.locals.startTime = Date.now();
-    res.locals.errors = [];
-    next();
-})
-.listen(PORT, () => logger.info(`Server running on PORT ${PORT}`))
-
+server
+	.use(express.json())
+	.use((req, res, next) => {
+		res.append('Access-Control-Allow-Methods', 'OPTIONS');
+		res.append('Cache-Control', 'no-store');
+		next();
+	})
+	.use(router)
+	.use('/api', routes)
+	.use((req, res, next) => {
+		res.locals.startTime = Date.now();
+		res.locals.errors = [];
+		next();
+	})
+	.listen(PORT, () => logger.info(`Server running on PORT ${PORT}`));
